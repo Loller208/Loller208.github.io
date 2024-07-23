dc263e7cf9b71ebae6f9c255e16f75370b33e149
Loller208 <108628779+Loller208@users.noreply.github.com>
2024-07-23 12:18:26 +0200

    Update main_gltf.js

--- main_gltf.js
+++ main_gltf.js
@@ -36,7 +36,7 @@ function start_processing(){
     container.matrixAutoUpdate = false;
     scene.add(container);
     const loader = new GLTFLoader();
-    loader.load('tablev.glb', model => { 
+    loader.load('table0.glb', model => { 
         container.add(model.scene);
     });
     const light = new THREE.AmbientLight(0xffffff,10);
